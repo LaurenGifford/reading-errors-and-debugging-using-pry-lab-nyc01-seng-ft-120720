45aa56481da8d9da2a46e318a4b9ea2f995f6d46
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-11 18:40:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,14 +3,15 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
-star_date = generate_star_date
 
 def state_log(star_date)
  puts "Captain's Log, star date #{star_date}."
 end
 
+date = generate_star_date
+
 def engage
-  puts state_log(star_date)
+  puts state_log(date)
 end
 
 engage
